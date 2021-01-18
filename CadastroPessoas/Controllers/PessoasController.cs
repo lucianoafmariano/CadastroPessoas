62858ed18900d9a4691ca097196fdb7e7f8363ce
Luciano Mariano <lucianoafmariano@gmail.com>
2021-01-18 10:16:27 -0300

    Implementacao da acao para incluir pessoa

---
+++ CadastroPessoas/Controllers/PessoasController.cs
@@ -0,0 +1,44 @@
+﻿using CadastroPessoas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroPessoas.Controllers
+{
+    public class PessoasController : Controller
+    {
+        private readonly Contexto _contexto;
+
+        public PessoasController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _contexto.Pessoas.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult CriarPessoa()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarPessoa(Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Add(pessoa);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            else return View(pessoa);
+        }
+    }
+}
