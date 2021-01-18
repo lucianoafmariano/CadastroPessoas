965684e900330a3e8fc8cfddf5c70e9c5b83f44e
Luciano Mariano <lucianoafmariano@gmail.com>
2021-01-18 10:06:46 -0300

    Implementacao do controller PessoasController

---
+++ CadastroPessoas/Controllers/PessoasCoontroller.cs
@@ -0,0 +1,25 @@
+﻿using CadastroPessoas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroPessoas.Controllers
+{
+    public class PessoasCoontroller : Controller
+    {
+        private readonly Contexto _contexto;
+
+        public PessoasCoontroller(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _contexto.Pessoas.ToListAsync());
+        }
+    }
+}
